7b530cfaaa9a6a2a5af3a0d5cccdd678bf9173bc
Rory Deken <rorydeken@gmail.com>
2017-04-21 10:07:00 -0400

    Added starter files

---
+++ bvg/generator.js
@@ -0,0 +1,6 @@
+
+var book, chapter, verse;
+
+// Randomize through books and then chapters and then verse each with a random selection
+
+// Then open a new tab/window and take the user to that site with a url that is built based on their selection
