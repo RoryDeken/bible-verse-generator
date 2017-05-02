c96e66742f2796871bb42c9608efb08725de7f21
Deken <deken.7@C-C02P37U0G3QD.local>
2017-05-02 14:38:29 -0400

    added form options and basic input from user.

--- docs/generator.js
+++ docs/generator.js
@@ -1,6 +1,22 @@
 
-var book, chapter, verse;
+var version, section, size, includeTxt, button;
+
+button = document.getElementById("start");
+button.addEventListener("click", run);
+
+function run(){
+version = document.getElementById('version').value;
+section = document.getElementById('section').value;
+size = document.getElementById('size').value;
+includeTxt = document.getElementById('includeTxt').checked;
+
+alert(version + " " + section + " " + size + " " + includeTxt);
+
+}
+
+
 
 // Randomize through books and then chapters and then verse each with a random selection
+
 
 // Then open a new tab/window and take the user to that site with a url that is built based on their selection
