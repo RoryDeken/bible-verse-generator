d54a767be60050d42ba4c5cde5bdd43632697b4c
Deken <deken.7@c-c02p37u0g3qd.ud.osu.edu>
2017-05-03 08:18:32 -0400

    Added ajax logic

--- docs/generator.js
+++ docs/generator.js
@@ -4,6 +4,29 @@ var version, section, size, includeTxt, button, text = document.getElementById('
 button = document.getElementById("start");
 button.addEventListener("click", run);
 
+
+var xmlhttp = new XMLHttpRequest();
+var url = "/versions/nasb.json";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<a href="' + arr[i].url + '">' +
+        arr[i].display + '</a><br>';
+    }
+    document.getElementById("output").innerHTML = out;
+}
+
 function run(event){
   event.preventDefault();
 version = document.getElementById('version').value;
