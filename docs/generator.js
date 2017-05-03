e7b78fdedef57e2992a17b5d7ee82a3255da96f8
Deken <deken.7@c-c02p37u0g3qd.ud.osu.edu>
2017-05-03 08:43:47 -0400

    Testing build

--- docs/generator.js
+++ docs/generator.js
@@ -5,15 +5,7 @@ button = document.getElementById("start");
 button.addEventListener("click", run);
 
 
-var xmlhttp = new XMLHttpRequest();
-var url = "/versions/nasb.json";
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
-    }
-};
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
@@ -35,6 +27,16 @@ size = document.getElementById('size').value;
 includeTxt = document.getElementById('includeTxt').checked;
 
 text.textContent = version + " " + section + " " + size + " " + includeTxt;
+
+var xmlhttp = new XMLHttpRequest();
+var url = "/versions/nasb.json";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+    }
+};
 
 }
 
