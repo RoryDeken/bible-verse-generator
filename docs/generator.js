b441132b6f3b3232bea08ebc627581c4282ff7fd
Deken <deken.7@C-C02P37U0G3QD.local>
2017-05-05 21:32:30 -0400

    added link to read text on bible gateway

--- docs/generator.js
+++ docs/generator.js
@@ -1,5 +1,5 @@
 
-var data, version,url, section, size, includeTxt, versions = ["nasb","niv","nlt"], options = ["nasb.json","niv.json", "nlt.json"], button, text = document.getElementById('text');
+var data, version,url, section, dest, size, includeTxt, versions = ["nasb","niv","nlt"], options = ["nasb.json","niv.json", "nlt.json"], button, link = document.getElementById('link'), text = document.getElementById('text');
 
 button = document.getElementById("start");
 button.addEventListener("click", run);
@@ -15,18 +15,32 @@ xmlhttp.onreadystatechange = function() {
         var randomBook = Math.floor(Math.random() * (data.length - 0)) + 0;
         var randomChap = Math.floor(Math.random() * ( data[randomBook].chapter.length - 0)) + 0;
         var randomVerse = Math.floor(Math.random() * (data[randomBook].chapter[randomChap].verse.length - 0)) + 0;
-      text.innerHTML = data[randomBook].name + " " + data[randomBook].chapter[randomChap].name + " " + data[randomBook].chapter[randomChap].verse[randomVerse].name;
-var dest = "https://www.biblegateway.com/passage/?search="+ data[randomBook].name + "+" + data[randomBook].chapter[randomChap].name + "%3A" + data[randomBook].chapter[randomChap].verse[randomVerse].name + "&version=" + versions[version - 1 ];
-window.open(dest);
+      text.innerHTML = data[randomBook].name + " " + data[randomBook].chapter[randomChap].name + ":" + data[randomBook].chapter[randomChap].verse[randomVerse].name;
+      link.setAttribute("href", dest);
+// window.open(dest);
 
+switch(size){
+
+case 0: dest = "https://www.biblegateway.com/passage/?search="+ data[randomBook].name + "+" + data[randomBook].chapter[randomChap].name + "%3A" + data[randomBook].chapter[randomChap].verse[randomVerse].name + "&version=" + versions[version - 1 ];
+break;
+
+case 1: dest = "https://www.biblegateway.com/passage/?search="+ data[randomBook].name + "+" + data[randomBook].chapter[randomChap].name + "&version=" + versions[version - 1 ];
+break;
+
+case 2: dest = "https://www.biblegateway.com/passage/?search="+ data[randomBook].name + "&version=" + versions[version - 1 ];
+
+
+}
+
+link.setAttribute("href", dest);
     }
 };
 
 function run(event){
 event.preventDefault();
 version = parseInt(document.getElementById('version').value);
-section = document.getElementById('section').value;
-size = document.getElementById('size').value;
+section = document.getElementById('section').value - 1;
+size = document.getElementById('size').value - 1;
 includeTxt = document.getElementById('includeTxt').checked;
 console.log(url);
 
