4fb57038b1e5b3be9254c2e4034f5b272474a6d9
Deken <deken.7@C-C02P37U0G3QD.local>
2017-05-05 20:44:52 -0400

    added logic to randomize chapters and verses

--- docs/generator.js
+++ docs/generator.js
@@ -16,7 +16,7 @@ xmlhttp.onreadystatechange = function() {
         var randomChap = Math.floor(Math.random() * ( data[randomBook].chapter.length - 0)) + 0;
         var randomVerse = Math.floor(Math.random() * (data[randomBook].chapter[randomChap].verse.length - 0)) + 0;
       console.log(data[randomBook].name + " " + data[randomBook].chapter[randomChap].name + " " + data[randomBook].chapter[randomChap].verse[randomVerse].name);
-var dest = "https://www.biblegateway.com/passage/?search="+ data[randomBook].name + "+" + data[randomBook].chapter[randomChap] + "%3A" + data[randomBook].chapter[randomChap].verse[randomVerse] + "&version=" + version;
+var dest = "https://www.biblegateway.com/passage/?search="+ data[randomBook].name + "+" + data[randomBook].chapter[randomChap].name + "%3A" + data[randomBook].chapter[randomChap].verse[randomVerse].name + "&version=" + version;
 window.open(dest);
     }
 };
