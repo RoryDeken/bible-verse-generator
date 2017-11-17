6a5bd37481f5a612ecaba17f4672ce514b9320d6
Deken <deken.7@c-c02p37u0g3qd.ud.osu.edu>
2017-11-17 15:04:01 -0500

    Fixed error with array type

--- docs/generator.js
+++ docs/generator.js
@@ -40,8 +40,8 @@ switch(section){
   default: randomBook =  data[generateRand(data.length)];
 }
 
-randomChap =   randomBook.chapter.isArray() ? generateRand(randomBook.chapter.length) : randomBook.chapter;
-randomVerse =  randomBook.chapter.isArray() ? generateRand(randomBook.chapter[randomChap].verse.length) : generateRand(randomBook.chapter.verse.length);
+randomChap =   Array.isArray(randomBook.chapter) ? generateRand(randomBook.chapter.length): randomBook.chapter;
+randomVerse =  Array.isArray(randomBook.chapter) ? generateRand(randomBook.chapter[randomChap].verse.length) : generateRand(randomBook.chapter.verse.length);
 
 
 switch(size){
